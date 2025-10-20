cc18974b514fb6e2b92a3f427913df80bc62e2df
Ashwin Kumar <kumar.ashwin@outlook.com>
2025-10-20 23:28:15 +0900

    More fundamental functioning figured out

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const { createServer } = require('node:http');
-const fs = require('fs');
+import { createServer } from 'node:http';
+import fs from 'fs';
+import path from 'path';
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const httpHandler = (req, res) => {
-  fs.readFile('./public/index.html', (err, data) => {
-    if (err == null) {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
+  // Resolve file path (prevent directory traversal)
+  let filePath = path.join("./public", req.url === "/" ? "index.html" : req.url);
+
+  // Detect content type
+  const ext = path.extname(filePath);
+  let contentType = "text/html";
+  if (ext === ".js") contentType = "text/javascript";
+  else if (ext === ".css") contentType = "text/css";
+  else if (ext === ".json") contentType = "application/json";
+  else if (ext === ".png") contentType = "image/png";
+  else if (ext === ".jpg") contentType = "image/jpeg";
+
+  fs.readFile(filePath, (err, data) => {
+    if (!err) {
+      res.writeHead(200, { 
+        "Content-Type": contentType,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":"GET",
+        "Access-Control-Allow-Headers": "*" 
+      });
       res.end(data);
+    } else if (err.code === "ENOENT") {
+      fs.readFile("./public/index.html", (_, _data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(_data);
+      });
     } else {
-      res.statusCode = 500;
-      console.log(err);
-      res.end('Internal Server Error');
+      res.writeHead(500);
+      res.end("Internal Server Error");
     }
   });
 };
