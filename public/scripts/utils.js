66310ea4b798f67f5fcb9608ddbf86b2c4d7cab0
Ashwin Kumar <kumar.ashwin@outlook.com>
2025-10-23 00:40:40 +0900

    Rearranged files and also added some code to work with queryParams

---
+++ public/scripts/utils.js
@@ -0,0 +1,60 @@
+let kanjiMeaningsDict;
+export const fetchKanjiMeanings = async () => {
+  try {
+    const response = await fetch('./data/kanji-meanings-dict.json')
+    kanjiMeaningsDict = await response.json();
+    console.log(kanjiMeaningsDict);
+  } catch (error) {
+    console.error('Error loading Kanji Meanings:', error);
+  }
+}
+
+const getMeaningsForKanji = (text) => {
+  let meanings = {};
+  for (const character of text) {
+    const kanjiMeanings = kanjiMeaningsDict[character];
+    if (!!Object.keys(kanjiMeanings)) {
+      meanings[character] = kanjiMeanings["Meanings"];
+    }
+  }
+  return meanings;
+};
+
+export const populateMeaningsLists = () => {
+  clearQueryParams();
+  const meaningsLists = document.getElementById('meanings-lists');
+  const textInput = document.getElementById('search-field');
+  const kanjiMeanings = getMeaningsForKanji(textInput.value);
+
+  // Clear existing table content and repopulate table with kanji meanings
+  if (!!meaningsLists) meaningsLists.innerHTML = '';
+  Object.keys(kanjiMeanings).forEach((kanji, kanjiIndex) => {
+    const ul = document.createElement('ul');
+    for (const meaning of kanjiMeanings[kanji]) {
+      if (!!String(meaning).trim().length) {
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.textContent = meaning;
+        button.onclick = () => appendQueryParam(`kanji-${kanjiIndex + 1}`, meaning);
+        li.append(button);
+        ul.append(li);
+      } else {
+        break;
+      }
+    }
+    meaningsLists.appendChild(ul);
+  });
+};
+
+export const appendQueryParam = (paramName, paramValue) => {
+  const url = new URL(window.location.href);
+  url.searchParams.set(paramName, paramValue);
+  history.pushState({}, "", url);
+}
+
+export const clearQueryParams = () => {
+  const url = new URL(window.location.href);
+  url.search = "";
+  history.pushState({}, "", url);
+}
+ 
